cfbc97924ec5ec08153ccb28b597375a3f056811
Gaurav153fr <118659860+Gaurav153fr@users.noreply.github.com>
2024-05-22 10:34:48 +0530

    fixed og i guess

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-// app/layout.tsx
 import { Providers } from "./providers";
 import NavbarHead from "./components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Faceless-avatar ",
-  description: " Create Your Avatar! 🎨✨",
+  title: "Faceless-avatar",
+  description: "Create Your Avatar! 🎨✨",
 };
 
 export default function RootLayout({
@@ -19,14 +18,19 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <link rel="icon" href="fav.png" sizes="any" />
-      <meta property="og:image" content="/og.jpg" />
+      <meta property="og:title" content="Faceless-avatar" />
+      <meta property="og:description" content="Create Your Avatar! 🎨✨" />
+      <meta property="og:image" content="og.jpg" />
       <meta
         property="og:image:alt"
-        content="faceless avatar- create yourn own avataar"
+        content="Faceless Avatar - Create Your Own Avatar"
       />
-      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:type" content="image/jpg" />
       <meta property="og:image:width" content="1280" />
       <meta property="og:image:height" content="720" />
+      <meta property="og:url" content="https://faceless-avatar.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta name="theme-color" content="#000000" />
       <body className={inter.className}>
         <Providers>
           <NavbarHead />
